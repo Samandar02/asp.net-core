7757e91c7f1a396029bc5d0e3a632400b53ff3bb
Samandar <uchqunovsamandar@mail.ru>
2022-06-25 11:42:52 -0700

    initial commit

---
+++ Asp.Net Core Identity practice/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+    }
+}
+public static class UserRoles
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+}
